3dd8460b7c8e9d8336e1f5be17dbf5935e6354ee
Björn Scheppler <scep@zhaw.ch>
2018-11-21 18:39:53 +0100

    Typo

--- src/main/java/ch/zhaw/gpi/veka/RestServiceTemplateApplication.java
+++ src/main/java/ch/zhaw/gpi/veka/RestServiceTemplateApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hauptklasse für die VeKa-SpringBoot-Applikation
+ * Hauptklasse für die RestService-Template-SpringBoot-Applikation
  * 
  * @author scep
  */
